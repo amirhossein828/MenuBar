36e1b80fc29744d9da182773eee97b9e15316d00
Amir <hashemi.eng1985@gmail.com>
2017-10-28 14:03:59 -0400

    First commit. Create MenuBar class

---
+++ HiSwift/MenuBar.swift
@@ -0,0 +1,9 @@
+//
+//  MenuBar.swift
+//  HiSwift
+//
+//  Created by seyedamirhossein hashemi on 2017-10-28.
+//  Copyright © 2017 seyedamirhossein hashemi. All rights reserved.
+//
+
+import Foundation
