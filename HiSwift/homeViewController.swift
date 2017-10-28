7f31299b0da704535a5db0c7fa0ef08d4c27c14e
Amir <hashemi.eng1985@gmail.com>
2017-10-28 14:02:38 -0400

    Initial Commit

---
+++ HiSwift/homeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HiSwift
+//
+//  Created by seyedamirhossein hashemi on 2017-10-28.
+//  Copyright © 2017 seyedamirhossein hashemi. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
