763034e67ff398aaaa7a14f9cf59a676dbf4a425
Amir <hashemi.eng1985@gmail.com>
2017-11-11 22:48:09 -0500

    add page controller to the Screen.

---
+++ Hiswift/MyPageViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MyPageViewController.swift
+//  MenuBar
+//
+//  Created by seyedamirhossein hashemi on 2017-11-11.
+//  Copyright © 2017 seyedamirhossein hashemi. All rights reserved.
+//
+
+import UIKit
+
+class MyPageViewController: UIViewController {
+
+    @IBOutlet weak var myPageController: UIPageControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
+
