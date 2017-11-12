52967d0d3f22fed53d3e8dd5fa868ee56740ccd4
Amir <hashemi.eng1985@gmail.com>
2017-11-11 23:51:48 -0500

    use another view controller to have two container view inside it to show page contoller

--- Hiswift/MyPageViewController.swift
+++ Hiswift/MyPageViewController.swift
@@ -9,18 +9,33 @@
 import UIKit
 
 class MyPageViewController: UIViewController {
+    
+    var delegate : HomeSelectItemOfPageControllerDelegate?
 
     @IBOutlet weak var myPageController: UIPageControl!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.delegate = self
 
-        // Do any additional setup after loading the view.
+    }
+    func myPageControllerFunc(item : Int){
+        self.myPageController.currentPage = item
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
+}
+
+extension MyPageViewController : HomeSelectItemOfPageControllerDelegate {
+    func selectItem(item : Int) {
+        self.myPageController.currentPage = item
+    }
+}
+
+protocol HomeSelectItemOfPageControllerDelegate : class {
+    func selectItem(item : Int)
 }
 
 
